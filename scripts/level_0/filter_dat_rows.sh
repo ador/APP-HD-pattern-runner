d6e1c7f6c0798ff08540b60d228ecbb05d3a853d
Adrienn Szabo <adrienn.szabo4@gmail.com>
2015-02-23 13:35:02 +0100

    Fixing config for level 0 and acript refactor

--- scripts/level_0/filter_dat_rows.sh
+++ scripts/level_0/filter_dat_rows.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
 # Note: paths should be absolute
+dockerConfPath=/home/yoda/git/APP-HD-pattern-runner/configs/level_0/filter_dat_rows.props
+dockerProtGitPath=/home/yoda/git/ProteinPatternSearch
+thisDir=$(dirname $0) || false
 
-# checking number of params
-if [[ $# -ne 2 ]] ; then
-  echo "Sample usage : ./filter_dat_rows.sh <absolute path to ProteinPatternSearch repo> <abs. path to properties file>"
+errMsg1a="Sample usage 1 (if in docker environment: no params):"
+errMsg1b=" ./filter_dat_rows.sh"
+errMsg2a="Sample usage 2 (two params are needed):"
+errMsg2b=" ./filter_dat_rows.sh <absolute path to ProteinPatternSearch repo> <absolute path to properties file>"
+
+# Branching on number of params: default values are for the docker image
+if [[ $# -eq 0 ]] ; then
+  protkaDir=$dockerProtGitPath
+  confFile=$dockerConfPath
+fi
+
+if [[ $# -eq 1 ]] ; then
+  echo ${errMsg1a} && echo ${errMsg1b} && echo ${errMsg2a} && echo ${errMsg2b}
   exit 2;
 fi
 
-thisDir=$(dirname $0) || false
-protkaDir=$1
-confFile=$2
 
+if [[ $# -eq 2 ]] ; then
+  protkaDir=$1
+  confFile=$2
+fi
+
+if [[ $# -gt 2 ]] ; then
+  echo ${errMsg1a} && echo ${errMsg1b} && echo ${errMsg2a} && echo ${errMsg2b}
+  exit 2;
+fi
+
+# Running
 pushd ${protkaDir} > /dev/null
 
   java -jar java/build/libs/PPsearch.jar FILTER_ROWS $confFile
