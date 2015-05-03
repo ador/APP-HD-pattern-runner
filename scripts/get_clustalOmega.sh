8e1e4678d17f62a45a4706bbc9940ee093f8dacb
Adrienn Szabo <adrienn.szabo4@gmail.com>
2015-05-03 18:42:15 +0200

    scripts to get runnable tools

---
+++ scripts/get_clustalOmega.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+wget http://www.clustal.org/omega/clustalo-1.2.0-Ubuntu-x86_64
+chmod a+x clustalo-1.2.0-Ubuntu-x86_64
+
--- scripts/get_kClust.sh
+++ scripts/get_kClust.sh
@@ -1,2 +1,3 @@
 #!/bin/bash 
 wget ftp://toolkit.lmb.uni-muenchen.de/pub/kClust/kClust
+chmod a+x kClust
