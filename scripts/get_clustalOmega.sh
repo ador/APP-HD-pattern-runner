dd8cb4b5184a31ce86a5e13951fd0a8293c1a54b
Adrienn Szabo <adrienn.szabo4@gmail.com>
2015-05-03 19:15:15 +0200

    scripts to get tools moved

--- scripts/get_clustalOmega.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-wget http://www.clustal.org/omega/clustalo-1.2.0-Ubuntu-x86_64
-chmod a+x clustalo-1.2.0-Ubuntu-x86_64
-
---
+++ tools/get_clustalOmega.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+wget http://www.clustal.org/omega/clustalo-1.2.0-Ubuntu-x86_64 -O clustalo
+chmod a+x clustalo
+
