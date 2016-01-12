260b98d297e6abd486a8e11379dfe1b3100b7a22
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-01-12 13:55:12 +0100

    fixing Clustal downloader script

--- tools/get_clustalOmega.sh
+++ tools/get_clustalOmega.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-targetDir=$1
+targetDir="$HOME"/$1
+mkdir -p ${targetDir}
 wget http://www.clustal.org/omega/clustalo-1.2.0-Ubuntu-x86_64 -O ${targetDir}/clustalo
 chmod a+x ${targetDir}/clustalo
-
