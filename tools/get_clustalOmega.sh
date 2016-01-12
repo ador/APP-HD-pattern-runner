7b13460a94f9a94041a7bde560103e0be0f3c410
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-01-12 14:01:25 +0100

    refactoring Clustal downloader script

--- tools/get_clustalOmega.sh
+++ tools/get_clustalOmega.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
-targetDir="$HOME"/$1
+
+targetDir="$HOME"/tools
+
+if [[ $# -gt 0 ]] ; then
+  targetDir="$HOME"/$1
+fi
+
 mkdir -p ${targetDir}
 wget http://www.clustal.org/omega/clustalo-1.2.0-Ubuntu-x86_64 -O ${targetDir}/clustalo
 chmod a+x ${targetDir}/clustalo
